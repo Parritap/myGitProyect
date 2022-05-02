28fe3a30d3169dbc6599cab5bf8ed60ed3c8788b
Juan Esteban Parra Parra <estebanparra946@gmail.com>
2022-05-01 20:19:35 -0500

    Primer comit de la branch -ONE-

--- src/mainModule/MainClass.java
+++ src/mainModule/MainClass.java
@@ -1,16 +1,12 @@
 package mainModule;
 
-import java.util.Iterator;
-
 public class MainClass {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Mensaje del Commit 2");
 		
-		for (int i = 0; i < 3; i++) {
-			System.out.println(i);
-		}
+		
 
 	}
 
---
+++ src/mainModule/branchOneClass.java
@@ -0,0 +1,12 @@
+package mainModule;
+
+public class branchOneClass {
+
+	public static void main(String[] args) {
+		
+		
+	//Este comentario indica que este documento fue creado en la branch "one".
+
+	}
+
+}
